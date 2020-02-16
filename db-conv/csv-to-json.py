20a3e703d64f4d22f3564595cd88ed35774ba72d
Andrew Wyatt <andywyatte17@gmail.com>
2020-02-16 20:52:32 +0000

    Tidyup csv-to-json.py

--- db-conv/csv-to-json.py
+++ db-conv/csv-to-json.py
@@ -1,35 +1,34 @@
 import csv
 import json
-import sys
+import sys, os
 import io
+import subprocess
 
-'''
-'''
-
-#Read CSV File
-def read_CSV(csvfile, json_file):
+def read_CSV(csvfile):
     csv_rows = []
     reader = csv.DictReader(csvfile)
     field = reader.fieldnames
     for row in reader:
-        csv_rows.extend([{field[i]:row[field[i]] for i in range(len(field))}])
-    convert_write_json(csv_rows, json_file)
+        stuff = {field[i]:row[field[i]] for i in range(len(field))}
+        csv_rows.append(stuff)
+    return csv_rows
 
-#Convert csv data into json
 def convert_write_json(data, json_file):
     f = json_file
     f.write(json.dumps(data, sort_keys=False, \
             indent=2, separators=(',', ': ')).encode('utf-8'))
 
+fiS = None
 for encoding in ('utf-8', 'cp1252'):
     try:
         with open(sys.argv[1], "rb") as fi0:
-            s = fi0.read().decode(encoding)
+            fiS = fi0.read().decode(encoding)
     except:
         continue
     break
 
-fi = io.StringIO(s)
-with open(sys.argv[2], "wb") as fo:
-    read_CSV(fi, fo)
+fi = io.StringIO(fiS)
+csv_rows = read_CSV(fi)
 
+with open(sys.argv[2], "wb") as fo:
+    convert_write_json(csv_rows, fo)
